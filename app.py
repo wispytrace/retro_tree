991dd9b4a0f0f9342f95917d15def10e962ffea5
wispytrace <1025489007@qq.com>
2026-07-31 15:17:48 +0800

    ok:

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from retro_service.api import app
+git pfrom retro_service.api import app
 from retro_service.schemas import PlanRequest
 from retro_service.orchestration import build_planner
 from retro_service.formatter import format_to_test_json
@@ -7,4 +7,4 @@ from retro_service.formatter import format_to_test_json
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=7755, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=9998, reload=True)
